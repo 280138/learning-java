b303d94138379ad9f9be82e060b8a854520ab46e
Cc <2801387084@qq.com>
2025-07-27 17:20:48 +0800

    111

---
+++ day01-helloworld-app/src/com/itheima/speedkey/SpeedKeyDemo.java
@@ -0,0 +1,11 @@
+package com.itheima.speedkey;
+
+public class SpeedKeyDemo {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+//        ctrl + D      复制到下一行
+    }
+}
