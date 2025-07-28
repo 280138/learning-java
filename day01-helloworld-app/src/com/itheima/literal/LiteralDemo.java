0689ca8e82e57d2fb5e616b1a9efe18b0d6ecdef
Cc <2801387084@qq.com>
2025-07-28 18:59:06 +0800

    literal

--- day01-helloworld-app/src/com/itheima/literal/LiteralDemo.java
+++ day01-helloworld-app/src/com/itheima/literal/LiteralDemo.java
@@ -1,10 +1,21 @@
 package com.itheima.literal;
-
+//目标：掌握常见自变量书写格式
 public class LiteralDemo {
     public static void main(String[] args) {
-        System.out.println(true);
-        System.out.println(false);
-        System.out.println(10);
-        System.out.println(3.14);
+        printLiteral();
     }
 }
+    public static void printLiteral(String[] args) {
+        System.out.println(true);
+        System.out.println(false);
+        System.out.println(100);
+        System.out.println(-100);
+        System.out.println(3.14);
+        System.out.println(-3.14);
+        System.out.println('a');
+        System.out.println('中');
+        System.out.println('\n');
+        System.out.println('\t');
+    }
+
+
